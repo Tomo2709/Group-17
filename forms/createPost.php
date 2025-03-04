a75aab82b0e0b95bdd9156f65e0b7fbe337beb09
Tomo2709 <CSMSTOML@ljmu.ac.uk>
2025-03-04 14:56:52 +0000

    forgot to push this?

--- forms/createPost.php
+++ forms/createPost.php
@@ -20,7 +20,8 @@ try{
     // uploads the file to directory and database
     move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile);
     $image = basename($targetFile);   
-    $GLOBALS["database"]->query("INSERT INTO `posts` (`message`, `thread`, `author`,`image`, `created`) VALUES ('$message', '$thread', '$author', '$image', '$created')");
+    $GLOBALS["database"]->query("INSERT INTO `posts` (`message`, `thread`, `author`,`image`, `created`) 
+    VALUES ('$message', '$thread', '$author', '$image', '$created')");
     header("Location: ../posts.php?thread=$thread");
 }
 catch(Exception){
